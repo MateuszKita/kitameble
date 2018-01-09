9d252f6f45d89ccab676927271f7f362c754a02a
Mateusz Kita <mkita.zg@gmail.com>
2018-01-09 14:11:01 +0100

    Dodoano loader w css zamiast gif

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.onreadystatechange = function () {
+document.onreadystatechange = () => {
     var state = document.readyState
     if (state == 'interactive') {
         document.getElementById('contents').style.visibility = "hidden";
     } else if (state == 'complete') {
-        setTimeout(function () {
+        setTimeout(() => {
             document.getElementById('interactive');
             document.getElementById('load').style.visibility = "hidden";
             document.getElementById('contents').style.visibility = "visible";
